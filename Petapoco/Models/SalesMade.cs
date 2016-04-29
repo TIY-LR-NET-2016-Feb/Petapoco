f29067cb46b9a571b4bcb09302212afa3a24a830
Brian Stickney <brianchase055@gmail.com>
2016-04-29 10:36:13 -0500

    SalesMade  and Sales Peolple Class

---
+++ Petapoco/Models/SalesMade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetaPoco;
+
+namespace Petapoco.Models
+{
+    [TableName("SalesMade")]
+    [PrimaryKey("Id")]
+    public class SalesMade
+    {
+        public SalesMade(string name, DateTime dateTime, int pretax)
+        {
+            this.Name = name;
+            this.SalesDate = dateTime;
+            this.PreTaxAmount = pretax;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime SalesDate { get; set; }
+        public int PreTaxAmount { get; set; }
+    }
+}
---
+++ Petapoco/Models/SalesPeople.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetaPoco;
+
+namespace Petapoco.Models
+{
+    [TableName("SalesPeople")]
+    [PrimaryKey("Id")]
+    public class SalesPeople
+    {
+
+        public SalesPeople(string name)
+        {
+            this.Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { set; get; }
+    }
+
+}
